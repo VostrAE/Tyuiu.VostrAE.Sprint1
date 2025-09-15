f0497b712bba6021fddbc697285d7385ab44a1c9
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-15 23:54:02 +0500

    Task2

---
+++ Tyuiu.VostrAE.Sprint1.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task2.V30.Lib;
+namespace Tyuiu.VostrAE.Sprint1.Task2.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ConvertKmToMetre()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertKmToMetre(x);
+            Assert.AreEqual(2000, res);
+      
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint1.Task2.V30/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task2.V30.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Вострых А.Е. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Выполнил: Вострых Александр Евгеньевич | РППб-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая переводит километры в метры                 *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите растояние в километрах:                                           *");
+        Console.WriteLine("***************************************************************************");
+        int x;
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Разультат  в метрах:                                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.ConvertKmToMetre(x));
+        Console.ReadLine();
+    }
+}
