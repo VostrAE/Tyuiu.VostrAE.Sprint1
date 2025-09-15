04d86e9d1cf96d5d80e292477ae797f98950312e
VostrAE <vostr50@gmail.com>
2025-09-15 20:19:03 +0500

    Update DataService.cs

--- Tyuiu.VostrAE.Sprint1.Task0.V5.Lib/DataService.cs
+++ Tyuiu.VostrAE.Sprint1.Task0.V5.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.VostrAE.Sprint1.Task0.V5.Lib
 {
-    public class DataService : ISprint1Task0V0
+    public class DataService : ISprint1Task0V5
     {
         public double Calculate()
         {
