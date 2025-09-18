e64513e689df96fd52d9f6de2e052d1124994c02
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-19 00:22:10 +0500

    Task3

---
+++ Tyuiu.VostrAE.Sprint1.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task3.V2.Lib;
+namespace Tyuiu.VostrAE.Sprint1.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void PurchaseAmount()
+        {
+            DataService ds = new DataService ();
+            double priceNotebook = 20;
+            int amountNotebook = 2;
+            double pricePencil = 5;
+            int amountPencil = 3;
+            double wait = 45;
+            var res = ds.PurchaseAmount(priceNotebook, amountNotebook, pricePencil, amountPencil);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
---
+++ Tyuiu.VostrAE.Sprint1.Task3.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task3.V2.Lib;
+
+class Program
+{static void Main(string[] args) { 
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Вострых А.Е. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Вострых Александр Евгеньевич | РППб-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу,которая запрашивает у пользователя исходные данные,  *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Введите цену тетради:                                                    *");
+        Console.WriteLine("***************************************************************************");
+        double priceNotebook;
+        priceNotebook = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Введите количество тетрадей:                                             *");
+        Console.WriteLine("***************************************************************************");
+        int amountNotebook;
+        amountNotebook = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Введите цену карандаша:                                                  *");
+        Console.WriteLine("***************************************************************************");
+        double pricePencil;
+        pricePencil = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Введите количество карандашей:                                           *");
+        Console.WriteLine("***************************************************************************");
+        int amountPencil;
+        amountPencil = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Разультат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Стоимость покуски составляет " + ds.PurchaseAmount(priceNotebook, amountNotebook, pricePencil, amountPencil));
+        Console.ReadLine();
+    }
+}
