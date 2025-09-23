38430727b5e730df1ce4b5121f157e3483bbc3cf
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-09-23 22:19:11 +0500

    Task5

---
+++ Tyuiu.VostrAE.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.VostrAE.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int k = 15;
+            DataService ds = new DataService();
+            double res = ds.Calculate (k);
+            int result = Convert.ToInt32(res);
+            int wait = 3;
+            Assert.AreEqual (wait, result);
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.VostrAE.Sprint1.Task5.V6.Lib;
+class Progaram
+{
+    static void Main(string[] args)
+    { DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(" Введите значение от 1 до 365:");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* В неделе этот день занимает место под номером:                          *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = Convert.ToInt32(ds.Calculate(k));
+        Console.WriteLine(res);
+        
+
+        Console.ReadKey();
+    }
+}
