d0bf97cdcc1847e3c6ca79eeb9c78a77a7200a1c
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-03 20:15:24 +0500

    1

--- Tyuiu.VostrAE.Sprint1.Task7.V8.Lib/DataService.cs
+++ Tyuiu.VostrAE.Sprint1.Task7.V8.Lib/DataService.cs
@@ -5,11 +5,8 @@ namespace Tyuiu.VostrAE.Sprint1.Task7.V8.Lib
     {
         public double Calculate(double x, double y)
         {
-            double numerator = y;
-            double denominator = Math.Cos(x) - x / 3.0;
-            double z = x * Math.Log(x) + numerator / denominator;
-            double res = x * Math.Log(x)+y/Math.Cos(x)-x/3;
-            return Math.Round(z,3);
+            double res = x * Math.Log(x)+(y/(Math.Cos(x)-x/3));
+            return Math.Round(res,3);
         }
     }
 }
